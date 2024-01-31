1b005391c581354e9ef9e167237bf7103bb4a2f9
Evans Sese <sese_evans@yahoo.com>
2024-01-31 18:54:27 +0300

    [Feature] Task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -15,21 +15,15 @@ class MRUCache(BaseCaching):
         """Put method implementation"""
         if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                """Remove the least recently used item"""
-                lru_key = self.order_of_access.pop(0)
-                print("DISCARD: {}".format(lru_key))
-                del self.cache_data[lru_key]
+                mru_key = list(self.cache_data.keys())[-1]
+                print("DISCARD: {}".format(mru_key))
+                del self.cache_data[mru_key]
 
             self.cache_data[key] = item
-            self.order_of_access.append(key)
 
     def get(self, key):
         """Get method implementation"""
         if key is not None:
-            if key in self.order_of_access:
-                self.order_of_access.remove(key)
-                self.order_of_access.append(key)
-
             return self.cache_data.get(key, None)
         else:
             return None
