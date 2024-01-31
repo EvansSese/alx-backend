808108c3d77f90b33b86420ffd466d9ba4043f94
Evans Sese <sese_evans@yahoo.com>
2024-01-31 18:37:07 +0300

    [Feature] Task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""MRU Cache class"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU Cache class"""
+    def __init__(self):
+        """Init method"""
+        super().__init__()
+        self.order_of_access = []
+
+    def put(self, key, item):
+        """Put method implementation"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                """Remove the least recently used item"""
+                lru_key = self.order_of_access.pop(0)
+                print("DISCARD: {}".format(lru_key))
+                del self.cache_data[lru_key]
+
+            self.cache_data[key] = item
+            self.order_of_access.append(key)
+
+    def get(self, key):
+        """Get method implementation"""
+        if key is not None:
+            if key in self.order_of_access:
+                self.order_of_access.remove(key)
+                self.order_of_access.append(key)
+
+            return self.cache_data.get(key, None)
+        else:
+            return None
