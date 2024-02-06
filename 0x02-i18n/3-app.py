d51171c37e85e910d3f37eb1ef0603199a43fb3a
Evans Sese <sese_evans@yahoo.com>
2024-02-06 12:57:14 +0300

    [Feature] Task 4

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Flask app with Babel"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    return render_template('0-index.html')
+
+
+app.config.from_object(Config)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Flask app with Babel"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    return render_template('0-index.html')
+
+
+app.config.from_object(Config)
+if __name__ == '__main__':
+    app.run(debug=True)
